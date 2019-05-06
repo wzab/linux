62a0f4fae95fc1456315381f313ec4e00734f481
Ezequiel Garcia <ezequiel@collabora.com>
2019-05-06 18:07:32 -0300

    TODO: add more modes into ov5647
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>

--- drivers/media/i2c/ov5647.c
+++ drivers/media/i2c/ov5647.c
@@ -36,15 +36,17 @@
 
 #define SENSOR_NAME "ov5647"
 
+#define REG_NULL 0xffff
+
 #define MIPI_CTRL00_CLOCK_LANE_GATE		BIT(5)
 #define MIPI_CTRL00_BUS_IDLE			BIT(2)
 #define MIPI_CTRL00_CLOCK_LANE_DISABLE		BIT(0)
 
 #define OV5647_SW_STANDBY		0x0100
 #define OV5647_SW_RESET			0x0103
-#define OV5647_REG_CHIPID_H		0x300A
-#define OV5647_REG_CHIPID_L		0x300B
-#define OV5640_REG_PAD_OUT		0x300D
+#define OV5647_REG_CHIPID_H		0x300a
+#define OV5647_REG_CHIPID_L		0x300b
+#define OV5647_REG_PAD_OUT		0x300d
 #define OV5647_REG_FRAME_OFF_NUMBER	0x4202
 #define OV5647_REG_MIPI_CTRL00		0x4800
 #define OV5647_REG_MIPI_CTRL14		0x4814
@@ -82,7 +84,8 @@ struct ov5647_mode {
 	unsigned int width;
 	unsigned int height;
 	unsigned int max_fps;
-	unsigned int pixel_rate;
+	unsigned int hts_def;
+	unsigned int vts_def;
 	const struct regval_list *reg_list;
 };
 
@@ -109,71 +112,34 @@ static struct regval_list sensor_oe_disable_regs[] = {
 	{0x3000, 0x00},
 	{0x3001, 0x00},
 	{0x3002, 0x00},
+	{REG_NULL, 0x00}
 };
 
 static struct regval_list sensor_oe_enable_regs[] = {
 	{0x3000, 0x0f},
 	{0x3001, 0xff},
 	{0x3002, 0xe4},
+	{REG_NULL, 0x00}
 };
 
-static struct regval_list ov5647_640x480[] = {
-	{0x0100, 0x00},
-	{0x0103, 0x01},
-	{0x3034, 0x08},
-	{0x3035, 0x21},
-	{0x3036, 0x46},
-	{0x303c, 0x11},
-	{0x3106, 0xf5},
-	{0x3821, 0x07},
-	{0x3820, 0x41},
-	{0x3827, 0xec},
+static struct regval_list ov5647_init_regs[] = {
+	/* upstream */
 	{0x370c, 0x0f},
-	{0x3612, 0x59},
-	{0x3618, 0x00},
 	{0x5000, 0x06},
-	{0x5001, 0x01},
-	{0x5002, 0x41},
 	{0x5003, 0x08},
 	{0x5a00, 0x08},
 	{0x3000, 0x00},
 	{0x3001, 0x00},
 	{0x3002, 0x00},
-	{0x3016, 0x08},
-	{0x3017, 0xe0},
-	{0x3018, 0x44},
-	{0x301c, 0xf8},
 	{0x301d, 0xf0},
 	{0x3a18, 0x00},
 	{0x3a19, 0xf8},
 	{0x3c01, 0x80},
 	{0x3b07, 0x0c},
-	{0x380c, 0x07},
-	{0x380d, 0x68},
-	{0x380e, 0x03},
-	{0x380f, 0xd8},
-	{0x3814, 0x31},
-	{0x3815, 0x31},
-	{0x3708, 0x64},
-	{0x3709, 0x52},
-	{0x3808, 0x02},
-	{0x3809, 0x80},
-	{0x380a, 0x01},
-	{0x380b, 0xE0},
-	{0x3801, 0x00},
-	{0x3802, 0x00},
-	{0x3803, 0x00},
-	{0x3804, 0x0a},
-	{0x3805, 0x3f},
-	{0x3806, 0x07},
-	{0x3807, 0xa1},
-	{0x3811, 0x08},
-	{0x3813, 0x02},
 	{0x3630, 0x2e},
 	{0x3632, 0xe2},
 	{0x3633, 0x23},
 	{0x3634, 0x44},
-	{0x3636, 0x06},
 	{0x3620, 0x64},
 	{0x3621, 0xe0},
 	{0x3600, 0x37},
@@ -187,12 +153,6 @@ static struct regval_list ov5647_640x480[] = {
 	{0x3f05, 0x02},
 	{0x3f06, 0x10},
 	{0x3f01, 0x0a},
-	{0x3a08, 0x01},
-	{0x3a09, 0x27},
-	{0x3a0a, 0x00},
-	{0x3a0b, 0xf6},
-	{0x3a0d, 0x04},
-	{0x3a0e, 0x03},
 	{0x3a0f, 0x58},
 	{0x3a10, 0x50},
 	{0x3a1b, 0x58},
@@ -200,21 +160,167 @@ static struct regval_list ov5647_640x480[] = {
 	{0x3a11, 0x60},
 	{0x3a1f, 0x28},
 	{0x4001, 0x02},
-	{0x4004, 0x02},
 	{0x4000, 0x09},
+	{0x5001, 0x01},		/* auto white balance */
+	{0x5002, 0x41},
+	{0x3011, 0x62},
+	/* mipi */
+	{0x3016, 0x08},
+	{0x3017, 0xe0},
+	{0x3018, 0x44},
+	{0x3034, 0x08},
+	{0x3106, 0xf5},
+	{REG_NULL, 0x00}
+};
+
+static struct regval_list ov5647_640x480[] = {
+	{0x3035, 0x21},
+	{0x3036, 0x60},
+	{0x303c, 0x11},
+	{0x3821, 0x07},
+	{0x3820, 0x41},
+	{0x3827, 0xec}, /* ? */
+	{0x370c, 0x0f}, /* ? */
+	{0x3612, 0x59},
+	{0x3618, 0x00},
+	{0x301c, 0xf8}, /* ? */
+	{0x380c, 0x07}, /* 0768 */
+	{0x380d, 0x68},
+	{0x380e, 0x03}, /* 03d8 */
+	{0x380f, 0xd8},
+	{0x3814, 0x31},
+	{0x3815, 0x31},
+	{0x3708, 0x64},
+	{0x3709, 0x52},
+	{0x3808, 0x02}, /* width = 640 */
+	{0x3809, 0x80},
+	{0x380a, 0x01}, /* heigth = 480 */
+	{0x380b, 0xE0},
+	{0x3800, 0x00},	/* X Start */
+	{0x3801, 0x00},
+	{0x3802, 0x00},
+	{0x3803, 0x00},
+	{0x3804, 0x0a},
+	{0x3805, 0x3f},
+	{0x3806, 0x07},
+	{0x3807, 0xa1},
+	{0x3811, 0x08},
+	{0x3813, 0x02},
+	{0x3636, 0x06},
+	{0x3a08, 0x01},
+	{0x3a09, 0x27},
+	{0x3a0a, 0x00},
+	{0x3a0b, 0xf6},
+	{0x3a0d, 0x04},
+	{0x3a0e, 0x03},
+	{0x4004, 0x02},
 	{0x4837, 0x24},
 	{0x4050, 0x6e},
 	{0x4051, 0x8f},
-	{0x0100, 0x01},
+	{REG_NULL, 0x00}
+};
+
+static struct regval_list ov5647_1296x972[] = {
+	{0x3035, 0x21},		/* PLL */
+	{0x3036, 0x60},		/* PLL */
+	{0x303c, 0x11},		/* PLL */
+	{0x3821, 0x07},		/* ISP mirror on, Sensor mirror on, H bin on */
+	{0x3820, 0x41},		/* ISP flip off, Sensor flip off, V bin on */
+	{0x3612, 0x59},		/* analog control */
+	{0x3618, 0x00},		/* analog control */
+	{0x380c, 0x07},		/* HTS = 1896 */
+	{0x380d, 0x68},		/* HTS */
+	{0x380e, 0x05},		/* VTS = 1420 */
+	{0x380f, 0x8c},		/* VTS */
+	{0x3814, 0x31},		/* X INC */
+	{0x3815, 0x31},		/* X INC */
+	{0x3708, 0x64},		/* analog control */
+	{0x3709, 0x52},		/* analog control */
+	{0x3808, 0x05},		/* DVPHO = 1296 */
+	{0x3809, 0x10},		/* DVPHO */
+	{0x380a, 0x03},		/* DVPVO = 984 */
+	{0x380b, 0xcc},		/* DVPVO */
+	{0x3800, 0x00},		/* X Start */
+	{0x3801, 0x08},		/* X Start */
+	{0x3802, 0x00},		/* Y Start */
+	{0x3803, 0x02},		/* Y Start */
+	{0x3804, 0x0a},		/* X End */
+	{0x3805, 0x37},		/* X End */
+	{0x3806, 0x07},		/* Y End */
+	{0x3807, 0xa1},		/* Y End */
+	/* banding filter */
+	{0x3a08, 0x01},		/* B50 */
+	{0x3a09, 0x27},		/* B50 */
+	{0x3a0a, 0x00},		/* B60 */
+	{0x3a0b, 0xf6},		/* B60 */
+	{0x3a0d, 0x04},		/* B60 max */
+	{0x3a0e, 0x03},		/* B50 max */
+	{0x4004, 0x02},		/* black line number */
+	{0x4837, 0x24},		/* MIPI pclk period */
+	{REG_NULL, 0x00}
+};
+
+static struct regval_list ov5647_2592x1944[] = {
+	{0x3035, 0x21},
+	{0x3036, 0x60},
+	{0x303c, 0x11},
+	{0x3612, 0x5b},
+	{0x3618, 0x04},
+	{0x380c, 0x0a},
+	{0x380d, 0x8c},
+	{0x380e, 0x07},
+	{0x380f, 0xb6},
+	{0x3814, 0x11},
+	{0x3815, 0x11},
+	{0x3708, 0x64},
+	{0x3709, 0x12},
+	{0x3808, 0x0a},
+	{0x3809, 0x20},
+	{0x380a, 0x07},
+	{0x380b, 0x98},
+	{0x3800, 0x00},
+	{0x3801, 0x0c},
+	{0x3802, 0x00},
+	{0x3803, 0x04},
+	{0x3804, 0x0a},
+	{0x3805, 0x33},
+	{0x3806, 0x07},
+	{0x3807, 0xa3},
+	{0x3a08, 0x01},
+	{0x3a09, 0x28},
+	{0x3a0a, 0x00},
+	{0x3a0b, 0xf6},
+	{0x3a0d, 0x08},
+	{0x3a0e, 0x06},
+	{0x4004, 0x04},
+	{0x4837, 0x19},
+	{REG_NULL, 0x00}
 };
 
 static const struct ov5647_mode supported_modes[] = {
 	{
 		.width = 640,
 		.height = 480,
-		.max_fps = 90,
-		.pixel_rate = 46500000,
+		.max_fps = 40, /* should be 60 but I get errors that way */
+		.hts_def = 0x0768,
+		.vts_def = 0x03d8,
 		.reg_list = ov5647_640x480,
+	},
+	{
+		.width = 1296,
+		.height = 972,
+		.max_fps = 25,
+		.hts_def = 0x0768,
+		.vts_def = 0x058c,
+		.reg_list = ov5647_1296x972,
+	},
+	{
+		.width = 2592,
+		.height = 1944,
+		.max_fps = 15,
+		.hts_def = 0x0a8c,
+		.vts_def = 0x07b6,
+		.reg_list = ov5647_2592x1944,
 	},
 };
 
@@ -254,17 +360,34 @@ static int ov5647_read(struct v4l2_subdev *sd, u16 reg, u8 *val)
 }
 
 static int ov5647_write_array(struct v4l2_subdev *sd,
-				struct regval_list *regs, int array_size)
+			      const struct regval_list *regs)
 {
 	int i, ret;
 
-	for (i = 0; i < array_size; i++) {
+	for (i = 0; regs[i].addr != REG_NULL; i++) {
 		ret = ov5647_write(sd, regs[i].addr, regs[i].data);
 		if (ret < 0)
 			return ret;
 	}
 
 	return 0;
+}
+
+static int ov5647_set_sw_standby(struct v4l2_subdev *sd, bool standby)
+{
+	int ret;
+	u8 rdval;
+
+	ret = ov5647_read(sd, OV5647_SW_STANDBY, &rdval);
+	if (ret < 0)
+		return ret;
+
+	if (standby)
+		rdval &= ~0x01;
+	else
+		rdval |= 0x01;
+
+	return ov5647_write(sd, OV5647_SW_STANDBY, rdval);
 }
 
 static int ov5647_set_virtual_channel(struct v4l2_subdev *sd, int channel)
@@ -287,17 +410,26 @@ static int ov5647_stream_on(struct v4l2_subdev *sd)
 	ret = ov5647_write(sd, OV5647_REG_MIPI_CTRL00, MIPI_CTRL00_BUS_IDLE);
 	if (ret < 0)
 		return ret;
+	/* TODO: Might need to sleep here */
 
 	ret = ov5647_write(sd, OV5647_REG_FRAME_OFF_NUMBER, 0x00);
 	if (ret < 0)
 		return ret;
 
-	return ov5647_write(sd, OV5640_REG_PAD_OUT, 0x00);
+	ret = ov5647_write(sd, OV5647_REG_PAD_OUT, 0x00);
+	if (ret < 0)
+		return ret;
+
+	return ov5647_set_sw_standby(sd, false);
 }
 
 static int ov5647_stream_off(struct v4l2_subdev *sd)
 {
 	int ret;
+
+	ret = ov5647_set_sw_standby(sd, true);
+	if (ret < 0)
+		return ret;
 
 	ret = ov5647_write(sd, OV5647_REG_MIPI_CTRL00, MIPI_CTRL00_CLOCK_LANE_GATE
 			   | MIPI_CTRL00_BUS_IDLE | MIPI_CTRL00_CLOCK_LANE_DISABLE);
@@ -308,40 +440,34 @@ static int ov5647_stream_off(struct v4l2_subdev *sd)
 	if (ret < 0)
 		return ret;
 
-	return ov5647_write(sd, OV5640_REG_PAD_OUT, 0x01);
-}
-
-static int set_sw_standby(struct v4l2_subdev *sd, bool standby)
-{
-	int ret;
-	u8 rdval;
-
-	ret = ov5647_read(sd, OV5647_SW_STANDBY, &rdval);
-	if (ret < 0)
-		return ret;
-
-	if (standby)
-		rdval &= ~0x01;
-	else
-		rdval |= 0x01;
-
-	return ov5647_write(sd, OV5647_SW_STANDBY, rdval);
+	return ov5647_write(sd, OV5647_REG_PAD_OUT, 0x01);
 }
 
 static int __sensor_init(struct v4l2_subdev *sd)
 {
 	int ret;
-	u8 resetval, rdval;
+	u8 resetval;
+	struct ov5647 *sensor = to_state(sd);
 	struct i2c_client *client = v4l2_get_subdevdata(sd);
 
-	ret = ov5647_read(sd, OV5647_SW_STANDBY, &rdval);
+	ret = ov5647_set_sw_standby(sd, true);
 	if (ret < 0)
 		return ret;
 
-	ret = ov5647_write_array(sd, ov5647_640x480,
-					ARRAY_SIZE(ov5647_640x480));
+	ret = ov5647_write(sd, OV5647_SW_RESET, 0x01);
+	if (ret < 0)
+		return ret;
+	msleep(50);
+
+	ret = ov5647_write_array(sd, ov5647_init_regs);
 	if (ret < 0) {
 		dev_err(&client->dev, "write sensor default regs error\n");
+		return ret;
+	}
+
+	ret = ov5647_write_array(sd, sensor->cur_mode->reg_list);
+	if (ret < 0) {
+		dev_err(&client->dev, "write sensor mode regs error\n");
 		return ret;
 	}
 
@@ -349,16 +475,7 @@ static int __sensor_init(struct v4l2_subdev *sd)
 	if (ret < 0)
 		return ret;
 
-	ret = ov5647_read(sd, OV5647_SW_STANDBY, &resetval);
-	if (ret < 0)
-		return ret;
-
-	if (!(resetval & 0x01)) {
-		dev_err(&client->dev, "Device was in SW standby");
-		ret = ov5647_write(sd, OV5647_SW_STANDBY, 0x01);
-		if (ret < 0)
-			return ret;
-	}
+	msleep(50);
 
 	/*
 	 * stream off to make the clock lane into LP-11 state.
@@ -383,8 +500,7 @@ static int ov5647_sensor_power(struct v4l2_subdev *sd, int on)
 			goto out;
 		}
 
-		ret = ov5647_write_array(sd, sensor_oe_enable_regs,
-				ARRAY_SIZE(sensor_oe_enable_regs));
+		ret = ov5647_write_array(sd, sensor_oe_enable_regs);
 		if (ret < 0) {
 			clk_disable_unprepare(ov5647->xclk);
 			dev_err(&client->dev,
@@ -402,16 +518,9 @@ static int ov5647_sensor_power(struct v4l2_subdev *sd, int on)
 	} else if (!on && ov5647->power_count == 1) {
 		dev_dbg(&client->dev, "OV5647 power off\n");
 
-		ret = ov5647_write_array(sd, sensor_oe_disable_regs,
-				ARRAY_SIZE(sensor_oe_disable_regs));
-
+		ret = ov5647_write_array(sd, sensor_oe_disable_regs);
 		if (ret < 0)
 			dev_dbg(&client->dev, "disable oe failed\n");
-
-		ret = set_sw_standby(sd, true);
-
-		if (ret < 0)
-			dev_dbg(&client->dev, "soft stby failed\n");
 
 		clk_disable_unprepare(ov5647->xclk);
 	}
@@ -473,39 +582,87 @@ static const struct v4l2_subdev_video_ops ov5647_subdev_video_ops = {
 	.s_stream =		ov5647_s_stream,
 };
 
-static int ov5647_set_get_fmt(struct v4l2_subdev *sd,
+static int ov5647_get_reso_dist(const struct ov5647_mode *mode,
+				struct v4l2_mbus_framefmt *framefmt)
+{
+	return abs(mode->width - framefmt->width) +
+	       abs(mode->height - framefmt->height);
+}
+
+static const struct ov5647_mode *
+ov5647_find_best_fit(struct v4l2_subdev_format *fmt)
+{
+	struct v4l2_mbus_framefmt *framefmt = &fmt->format;
+	int dist;
+	int cur_best_fit = 0;
+	int cur_best_fit_dist = -1;
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(supported_modes); i++) {
+		dist = ov5647_get_reso_dist(&supported_modes[i], framefmt);
+		if (cur_best_fit_dist == -1 || dist < cur_best_fit_dist) {
+			cur_best_fit_dist = dist;
+			cur_best_fit = i;
+		}
+	}
+
+	return &supported_modes[cur_best_fit];
+}
+
+static int ov5647_set_fmt(struct v4l2_subdev *sd,
 			  struct v4l2_subdev_pad_config *cfg,
 			  struct v4l2_subdev_format *fmt)
 {
 	struct ov5647 *ov5647 = to_state(sd);
 	const struct ov5647_mode *mode;
+	s64 pixel_rate;
 	int ret = 0;
 
 	mutex_lock(&ov5647->lock);
 
-	mode = &supported_modes[0];
+	mode = ov5647_find_best_fit(fmt);
 	fmt->format.code = MEDIA_BUS_FMT_SBGGR8_1X8;
 	fmt->format.width = mode->width;
 	fmt->format.height = mode->height;
 	fmt->format.field = V4L2_FIELD_NONE;
 	if (fmt->which == V4L2_SUBDEV_FORMAT_TRY) {
-#ifdef CONFIG_VIDEO_V4L2_SUBDEV_API
 		*v4l2_subdev_get_try_format(sd, cfg, fmt->pad) = fmt->format;
-#else
-		ret = -ENOTTY;
-		goto err_unlock;
-#endif
 	} else {
+		ov5647->cur_mode = mode;
+		pixel_rate = mode->vts_def * mode->hts_def * mode->max_fps;
 		ret = v4l2_ctrl_s_ctrl_int64(ov5647->pixel_rate,
-					     mode->pixel_rate);
+					     pixel_rate);
 		if (ret < 0)
 			goto err_unlock;
-		ov5647->cur_mode = mode;
 	}
 
 err_unlock:
 	mutex_unlock(&ov5647->lock);
+
 	return ret;
+}
+
+static int ov5647_get_fmt(struct v4l2_subdev *sd,
+			  struct v4l2_subdev_pad_config *cfg,
+			  struct v4l2_subdev_format *fmt)
+{
+	struct ov5647 *ov5647 = to_state(sd);
+	const struct ov5647_mode *mode = ov5647->cur_mode;
+
+	mutex_lock(&ov5647->lock);
+
+	if (fmt->which == V4L2_SUBDEV_FORMAT_TRY) {
+		fmt->format = *v4l2_subdev_get_try_format(sd, cfg, fmt->pad);
+	} else {
+		fmt->format.width = mode->width;
+		fmt->format.height = mode->height;
+		fmt->format.code = MEDIA_BUS_FMT_SBGGR8_1X8;
+		fmt->format.field = V4L2_FIELD_NONE;
+	}
+
+	mutex_unlock(&ov5647->lock);
+
+	return 0;
 }
 
 static int ov5647_enum_mbus_code(struct v4l2_subdev *sd,
@@ -541,8 +698,8 @@ static int ov5647_enum_frame_sizes(struct v4l2_subdev *sd,
 static const struct v4l2_subdev_pad_ops ov5647_subdev_pad_ops = {
 	.enum_mbus_code = ov5647_enum_mbus_code,
 	.enum_frame_size = ov5647_enum_frame_sizes,
-	.get_fmt = ov5647_set_get_fmt,
-	.set_fmt = ov5647_set_get_fmt,
+	.get_fmt = ov5647_get_fmt,
+	.set_fmt = ov5647_set_fmt,
 };
 
 static const struct v4l2_subdev_ops ov5647_subdev_ops = {
