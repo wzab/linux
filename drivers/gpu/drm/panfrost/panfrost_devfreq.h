2647dd5031dd4817be6cbdc3d6f75c69203a4a91
Ezequiel Garcia <ezequiel@collabora.com>
2019-05-13 12:58:15 -0300

    panfrost: Make devfreq truly optional
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>

--- drivers/gpu/drm/panfrost/panfrost_devfreq.h
+++ drivers/gpu/drm/panfrost/panfrost_devfreq.h
@@ -4,11 +4,25 @@
 #ifndef __PANFROST_DEVFREQ_H__
 #define __PANFROST_DEVFREQ_H__
 
+#if defined(CONFIG_PM_DEVFREQ)
 int panfrost_devfreq_init(struct panfrost_device *pfdev);
-
 void panfrost_devfreq_resume(struct panfrost_device *pfdev);
 void panfrost_devfreq_suspend(struct panfrost_device *pfdev);
-
 void panfrost_devfreq_record_transition(struct panfrost_device *pfdev, int slot);
+#else
+static inline int panfrost_devfreq_init(struct panfrost_device *pfdev)
+{
+	return 0;
+}
+
+static inline void panfrost_devfreq_resume(struct panfrost_device *pfdev)
+{}
+
+static inline void panfrost_devfreq_suspend(struct panfrost_device *pfdev)
+{}
+
+static inline void panfrost_devfreq_record_transition(struct panfrost_device *pfdev, int slot)
+{}
+#endif
 
 #endif /* __PANFROST_DEVFREQ_H__ */
