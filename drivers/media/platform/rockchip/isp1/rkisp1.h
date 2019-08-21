d6b80182db32b00f9fd4bcb1fdcf9b61b4c1fa62
Helen Koike <helen.koike@collabora.com>
2019-08-21 19:43:55 -0300

    tmp: update file description

--- drivers/media/platform/rockchip/isp1/rkisp1.h
+++ drivers/media/platform/rockchip/isp1/rkisp1.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
 /*
- * Rockchip isp1 driver
+ * Rockchip ISP1 Driver - ISP Subdevice header
  *
  * Copyright (C) 2017 Rockchip Electronics Co., Ltd.
  */
