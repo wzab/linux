9123532e21b9c375eeaefdae3926730c56c80509
Jacob Chen <jacob2.chen@rock-chips.com>
2019-11-06 16:27:14 -0300

    media: staging: rkisp1: add rockchip isp1 core driver
    Add the core driver for rockchip isp1.
    
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
    Signed-off-by: Yichong Zhong <zyc@rock-chips.com>
    Signed-off-by: Jacob Chen <cc@rock-chips.com>
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Signed-off-by: Allon Huang <allon.huang@rock-chips.com>
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    [fixed compilation and run time errors regarding new v4l2 async API]
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    [Add missing module device table]
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>
    [refactored for upstream]
    Signed-off-by: Helen Koike <helen.koike@collabora.com>
    
    Series-changes: 10
    - unsquash
    - update TODO
    
    Series-changes: 9
    - remove ctrl_handler from struct rkisp1_isp_subdev
    - replace v4l2_{dgb,info,warn,err} by dev_*
    - s/pr_info/dev_dbg
    - s/int size/unsigned int size
    - remove module param rkisp1_debug
    - coding style fixes
    - fix error in subdev_notifier_bound, check dphy before assigning to sensor->dphy
    - remove subdevs fixed size array from rkisp1_pipeline
    - remove sensors list, as it can be identified from the media topology
    - Kconfig: add COMPILE_TEST in the dependency
    - use v4l2_pipeline_pm_use and remove _isp_pipeline_s_power() and _subdev_set_power()
    - remove struct rkisp1_pipeline and retrieve pipeline information from the media framework
    - remove remaining rk3288 code
    - cache pixel rate control in sctruct sensor_async_subdev
    - remove enum rkisp1_sd_type
    - squash
    - move to staging
    
    Series-changes: 7
    - VIDEO_ROCKCHIP_ISP1 selects VIDEOBUF2_VMALLOC
    - add PHY_ROCKCHIP_DPHY as a dependency for VIDEO_ROCKCHIP_ISP1
    - Fix compilation and runtime errors due to bitrotting
    The code has bit-rotten since March 2018, fix compilation errors.
    The new V4L2 async notifier API requires notifiers to be initialized by
    a call to v4l2_async_notifier_init() before being used, do so.
    - Add missing module device table
    - use clk_bulk framework
    - add missing notifiers cleanups
    - s/strlcpy/strscpy
    - normalize bus_info name
    - fix s_stream error path, stream_cnt wans't being decremented properly
    - use devm_platform_ioremap_resource() helper
    - s/deice/device
    - redesign: remove mipi/csi subdevice, sensors connect directly to the
    isp subdevice in the media topology now.
    - remove "saved_state" member from rkisp1_stream struct
    - Reverse the order of MIs
    - Simplify MI interrupt handling
    Rather than adding unnecessary indirection, just use stream index to
    handle MI interrupt enable/disable/clear, since the stream index matches
    the order of bits now, thanks to previous patch. While at it, remove
    some dead code.
    - code styling and checkpatch fixes

---
+++ drivers/staging/media/rkisp1/dev.h
@@ -0,0 +1,67 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Rockchip ISP1 Driver - Base driver
+ *
+ * Copyright (C) 2017 Rockchip Electronics Co., Ltd.
+ */
+
+#ifndef _RKISP1_DEV_H
+#define _RKISP1_DEV_H
+
+#include <linux/clk.h>
+
+#include "capture.h"
+#include "rkisp1.h"
+#include "isp_params.h"
+#include "isp_stats.h"
+
+#define DRIVER_NAME "rkisp1"
+#define ISP_VDEV_NAME DRIVER_NAME  "_ispdev"
+#define SP_VDEV_NAME DRIVER_NAME   "_selfpath"
+#define MP_VDEV_NAME DRIVER_NAME   "_mainpath"
+#define DMA_VDEV_NAME DRIVER_NAME  "_dmapath"
+
+#define RKISP1_MAX_BUS_CLK	8
+
+/*
+ * struct sensor_async_subdev - Sensor information
+ * @mbus: media bus configuration
+ */
+struct sensor_async_subdev {
+	struct v4l2_async_subdev asd;
+	struct v4l2_mbus_config mbus;
+	unsigned int lanes;
+	struct v4l2_subdev *sd;
+	struct v4l2_ctrl *pixel_rate_ctrl;
+	struct phy *dphy;
+};
+
+/*
+ * struct rkisp1_device - ISP platform device
+ * @base_addr: base register address
+ * @active_sensor: sensor in-use, set when streaming on
+ * @isp_sdev: ISP sub-device
+ * @rkisp1_stream: capture video device
+ * @stats_vdev: ISP statistics output device
+ * @params_vdev: ISP input parameters device
+ */
+struct rkisp1_device {
+	void __iomem *base_addr;
+	int irq;
+	struct device *dev;
+	unsigned int clk_size;
+	struct clk_bulk_data clks[RKISP1_MAX_BUS_CLK];
+	struct v4l2_device v4l2_dev;
+	struct v4l2_ctrl_handler ctrl_handler;
+	struct media_device media_dev;
+	struct v4l2_async_notifier notifier;
+	struct sensor_async_subdev *active_sensor;
+	struct rkisp1_isp_subdev isp_sdev;
+	struct rkisp1_stream stream[RKISP1_MAX_STREAM];
+	struct rkisp1_isp_stats_vdev stats_vdev;
+	struct rkisp1_isp_params_vdev params_vdev;
+	struct media_pipeline pipe;
+	struct vb2_alloc_ctx *alloc_ctx;
+};
+
+#endif
