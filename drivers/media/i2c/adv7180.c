2bf5ee0423dc8b7b7e7c28eb29a856117cdf3af7
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-01-26 16:36:23 +0100

    Changed definition of the type of vidfeo source...

--- drivers/media/i2c/adv7180.c
+++ drivers/media/i2c/adv7180.c
@@ -1376,9 +1376,9 @@ static int adv7180_probe(struct i2c_client *client,
 	if (ret)
 		goto err_unregister_vpp_client;
 
-	sd->entity.type = MEDIA_ENT_T_V4L2_SUBDEV_SENSOR; //WZab
+	//sd->entity.type = MEDIA_ENT_T_V4L2_SUBDEV_SENSOR; //WZab
 	state->pad.flags = MEDIA_PAD_FL_SOURCE;
-	sd->entity.function = MEDIA_ENT_F_ATV_DECODER;
+	sd->entity.function = MEDIA_ENT_F_CAM_SENSOR; //MEDIA_ENT_F_ATV_DECODER;
 	ret = media_entity_pads_init(&sd->entity, 1, &state->pad);
 	if (ret)
 		goto err_free_ctrl;
