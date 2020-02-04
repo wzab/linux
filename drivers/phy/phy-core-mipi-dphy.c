ac00c622486647b278f90bb30a4f9a55d5e61f91
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-02-04 21:17:21 +0100

    Added debug messages in phy_mipi_dphy_config_validate to check why PHY configuration fails for OV5647.

--- drivers/phy/phy-core-mipi-dphy.c
+++ drivers/phy/phy-core-mipi-dphy.c
@@ -92,75 +92,97 @@ int phy_mipi_dphy_config_validate(struct phy_configure_opts_mipi_dphy *cfg)
 
 	ui = ALIGN(PSEC_PER_SEC, cfg->hs_clk_rate);
 	do_div(ui, cfg->hs_clk_rate);
-
-	if (cfg->clk_miss > 60000)
+	printk(KERN_ALERT "phy_mipi_dphy_config_validate, ui=%llu\n",ui);
+	if (cfg->clk_miss > 60000) {
+		printk(KERN_ALERT "Invalid clk_miss: %d\n",cfg->clk_miss);
 		return -EINVAL;
-
-	if (cfg->clk_post < (60000 + 52 * ui))
+	};
+	if (cfg->clk_post < (60000 + 52 * ui)) {
+		printk(KERN_ALERT "Invalid clk_post: %d\n",cfg->clk_post);
 		return -EINVAL;
-
-	if (cfg->clk_pre < 8000)
+	};
+	if (cfg->clk_pre < 8000) {
+		printk(KERN_ALERT "Invalid clk_pre: %d\n",cfg->clk_pre);
 		return -EINVAL;
-
-	if (cfg->clk_prepare < 38000 || cfg->clk_prepare > 95000)
+	};
+	if (cfg->clk_prepare < 38000 || cfg->clk_prepare > 95000) {
+		printk(KERN_ALERT "Invalid clk_prepare: %d\n",cfg->clk_prepare);
 		return -EINVAL;
-
-	if (cfg->clk_settle < 95000 || cfg->clk_settle > 300000)
+	};
+	if (cfg->clk_settle < 95000 || cfg->clk_settle > 300000) {
+		printk(KERN_ALERT "Invalid clk_settle: %d\n",cfg->clk_settle);
 		return -EINVAL;
-
-	if (cfg->clk_term_en > 38000)
+	};
+	if (cfg->clk_term_en > 38000) {
+		printk(KERN_ALERT "Invalid clk_term_en: %d\n",cfg->clk_term_en);
 		return -EINVAL;
-
-	if (cfg->clk_trail < 60000)
+	};
+	if (cfg->clk_trail < 60000) {
+		printk(KERN_ALERT "Invalid clk_trail: %d\n",cfg->clk_trail);
 		return -EINVAL;
-
+	};
 	if ((cfg->clk_prepare + cfg->clk_zero) < 300000)
+		printk(KERN_ALERT "Invalid clk_prepare+clk_zero: %d\n",cfg->clk_prepare + cfg->clk_zero);
 		return -EINVAL;
-
-	if (cfg->d_term_en > (35000 + 4 * ui))
+	};
+	if (cfg->d_term_en > (35000 + 4 * ui)) {
+		printk(KERN_ALERT "Invalid d_term_en: %d\n",cfg->d_term_en);
 		return -EINVAL;
-
-	if (cfg->eot > (105000 + 12 * ui))
+	};
+	if (cfg->eot > (105000 + 12 * ui)) {
+		printk(KERN_ALERT "Invalid eot: %d\n",cfg->eot);
 		return -EINVAL;
-
-	if (cfg->hs_exit < 100000)
+	};
+	if (cfg->hs_exit < 100000) {
+		printk(KERN_ALERT "Invalid hs_exit: %d\n",cfg->hs_exit);
 		return -EINVAL;
-
+	};
 	if (cfg->hs_prepare < (40000 + 4 * ui) ||
-	    cfg->hs_prepare > (85000 + 6 * ui))
+	    cfg->hs_prepare > (85000 + 6 * ui)) {
+		printk(KERN_ALERT "Invalid hs_prepare: %d\n",cfg->hs_prepare);
 		return -EINVAL;
-
-	if ((cfg->hs_prepare + cfg->hs_zero) < (145000 + 10 * ui))
+	};
+	if ((cfg->hs_prepare + cfg->hs_zero) < (145000 + 10 * ui)) {
+		printk(KERN_ALERT "Invalid hs_prepare+hs_zero: %d\n",cfg->hs_prepare + cfg->hs_zero);
 		return -EINVAL;
-
+	};
 	if ((cfg->hs_settle < (85000 + 6 * ui)) ||
-	    (cfg->hs_settle > (145000 + 10 * ui)))
+	    (cfg->hs_settle > (145000 + 10 * ui))) {
+		printk(KERN_ALERT "Invalid hs_settle: %d\n",cfg->hs_settle);
 		return -EINVAL;
-
-	if (cfg->hs_skip < 40000 || cfg->hs_skip > (55000 + 4 * ui))
+	};
+	if (cfg->hs_skip < 40000 || cfg->hs_skip > (55000 + 4 * ui)) {
+		printk(KERN_ALERT "Invalid hs_skip: %d\n",cfg->hs_skip);
 		return -EINVAL;
-
-	if (cfg->hs_trail < max(8 * ui, 60000 + 4 * ui))
+	};
+	if (cfg->hs_trail < max(8 * ui, 60000 + 4 * ui)) {
+		printk(KERN_ALERT "Invalid hs_trail: %d\n",cfg->hs_trail);
 		return -EINVAL;
-
-	if (cfg->init < 100)
+	};
+	if (cfg->init < 100) {
+		printk(KERN_ALERT "Invalid init: %d\n",cfg->init);
 		return -EINVAL;
-
+	};
 	if (cfg->lpx < 50000)
+		printk(KERN_ALERT "Invalid lpx: %d\n",cfg->lpx);
 		return -EINVAL;
-
+	};
 	if (cfg->ta_get != (5 * cfg->lpx))
+		printk(KERN_ALERT "Invalid ta_get: %d, lpx=%d\n",cfg->ta_get,cfg->lpx);
 		return -EINVAL;
-
-	if (cfg->ta_go != (4 * cfg->lpx))
+	};
+	if (cfg->ta_go != (4 * cfg->lpx)) {
+		printk(KERN_ALERT "Invalid ta_go: %d, lpx=%d\n",cfg->ta_go, cfg->lpx);
 		return -EINVAL;
-
-	if (cfg->ta_sure < cfg->lpx || cfg->ta_sure > (2 * cfg->lpx))
+	};
+	if (cfg->ta_sure < cfg->lpx || cfg->ta_sure > (2 * cfg->lpx)) {
+		printk(KERN_ALERT "Invalid ta_sure: %d, lpx=%d\n",cfg->ta_sure,cfg->lpx);
+		return -EINVAL;	
+	};
+	if (cfg->wakeup < 1000)	{
+		printk(KERN_ALERT "Invalid wakeup: %d\n",cfg->wakeup);
 		return -EINVAL;
-
-	if (cfg->wakeup < 1000)
-		return -EINVAL;
-
+	}
 	return 0;
 }
 EXPORT_SYMBOL(phy_mipi_dphy_config_validate);
